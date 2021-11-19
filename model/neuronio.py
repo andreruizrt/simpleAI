a932e9e717f136dd466aa2f3dae573eb28dae81c
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-11-19 02:41:55 -0300

    added types and set parameter qtdLigacoes

--- model/neuronio.py
+++ model/neuronio.py
@@ -1,6 +1,9 @@
+from numpy import double
+
+
 class Neuronio:
-  def __init__(self):
-    self.peso = 0.0
-    self.erro = 0.0
-    self.saida = 0.0
-    self.quantidadeLigacoes = 0
+  def __init__(self, quantidadeLigacoes: int = 0):
+    self.quantidadeLigacoes = quantidadeLigacoes
+    self.peso: list = []
+    self.erro: double = 0.0
+    self.saida: double = 0.0
