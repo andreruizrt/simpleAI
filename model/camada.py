6f06628b4e5305295e87f7d6fdef09b52d923f8a
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-11-19 02:39:17 -0300

    add: description and types

--- model/camada.py
+++ model/camada.py
@@ -1,6 +1,10 @@
 class Camada:
-  def __init__(self, quantidadeNeuronios=0):
-      neuronios = []
-      quantidadeNeuronios = quantidadeNeuronios
+  def __init__(self, quantidadeNeuronios: int = 0):
+    """[Criar uma camada]
+    :param quantidadeNeuronios (int, optional): [Quantidade neuronios da camada]. Padrão 0.
+    """
+    self.quantidadeNeuronios = quantidadeNeuronios
+    self.neuronios = []
+    
   
   
