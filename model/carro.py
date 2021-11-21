e1b7e312cfce9ac0bd6aea6139bd61d3a2e6ce0c
andreruizrt <andrelucasruizalmeida@gmail.com>
2021-11-21 00:54:44 -0300

    add: sprite ao carro e altera criacao

--- model/carro.py
+++ model/carro.py
@@ -1,28 +1,36 @@
 from numpy import double
 import constants.main as constants
 import model.redeNeural as rn
-class Carro:
-    def __init__(self, x, y: double, vx, vy: double,
-                 angulo: double, velocidade: double,
-                 colidiu: bool, queimado: bool,
-                 sprint: bool, tamanhoDNA: int,
-                 DNA: double, fitness: double
+import pygame as pg
+
+class Carro(pg.sprite.Sprite):
+    def __init__(self,
+                 x: double = 0.0, y: double = 0.0,
+                 vx: double = 0.0, vy: double = 0.0,
+                 angulo: double = 0.0,
+                 velocidade: double = 0.0,
+                 colidiu: bool = False,
+                 queimado: bool = False,
+                 sprite: bool = False,
+                 tamanhoDNA: int = 0,
+                 DNA: double = 0.0,
+                 fitness: double = 0.0
                  ):
+        pg.sprite.Sprite.__init__(self)
         self.x: double = x
         self.y: double = y
         self.vx: double = vx
         self.vy: double = vy
         self.angulo: double = angulo
         self.velocidade: double = velocidade
-        self.colidiu: bool = False
-        self.queimado: bool = False
+        self.colidiu: bool = colidiu
+        self.queimado: bool = queimado
 
-        self.sprite: int = 0
-        self.distanciaSensores: list() = list()
-        self.distanciaSensores[constants.CAR_BRAIN_QTD_INPUT-1] = 0.0
+        self.sprite: int = sprite
+        self.distanciaSensores: list = [[0.0]*(constants.CAR_BRAIN_QTD_INPUT-1)]
 
-        self.tamanhoDNA: int = 0
-        self.DNA: double = 0.0
-        self.fitness: double = 0.0
+        self.tamanhoDNA: int = tamanhoDNA
+        self.DNA: double = DNA
+        self.fitness: double = fitness
 
         self.cerebro: rn.RedeNeural = rn.RedeNeural()
