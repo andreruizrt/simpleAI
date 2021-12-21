777198e8d2ce924ea98e2c53b6760c0d16f932ee
Andre Ruiz <andrelucasruizalmeida@gmail.com>
2021-12-21 00:09:34 -0300

    feat: creates the main file

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from test import Test
+from log.simpleLog import logging
+
+pygame.init()
+pygame.font.init()
+
+
+if __name__ == "__main__":
+    # app = main()
+    pass
+    
+        
+
+
+    
+
